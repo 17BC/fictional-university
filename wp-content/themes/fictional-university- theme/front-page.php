c72bb63c6cad0be162848041acbb987bdb632570
Homer Cerrafon <bjyc17@yahoo.com>
2020-07-23 07:48:24 -1000

    Added wp_reset_postdata in front-page.php

--- wp-content/themes/fictional-university- theme/front-page.php
+++ wp-content/themes/fictional-university- theme/front-page.php
@@ -118,34 +118,26 @@
       <div class="glide__slides">
          
 
-      <div class="hero-slider__slide" style="background-image: url(<?php echo get_theme_file_uri('/images/bus.jpg'); ?>);">
-          <div class="hero-slider__interior container">
-            <div class="hero-slider__overlay">
-              <h2 class="headline headline--medium t-center">Free Transportation</h2>
-              <p class="t-center">.</p>
-              <p class="t-center no-margin"><a href="#" class="btn btn--blue">Learn more</a></p>
-            </div>
-          </div>
-        </div>
-        <div class="hero-slider__slide" style="background-image: url(<?php echo get_theme_file_uri('/images/apples.jpg'); ?>);">
-          <div class="hero-slider__interior container">
-            <div class="hero-slider__overlay">
-              <h2 class="headline headline--medium t-center">An Apple a Day</h2>
-              <p class="t-center">Our dentistry program recommends eating apples.</p>
-              <p class="t-center no-margin"><a href="#" class="btn btn--blue">Learn more</a></p>
-            </div>
-          </div>
-        </div>
-        <div class="hero-slider__slide" style="background-image: url(<?php echo get_theme_file_uri('/images/bread.jpg'); ?>);">
-          <div class="hero-slider__interior container">
-            <div class="hero-slider__overlay">
-              <h2 class="headline headline--medium t-center">Free Food</h2>
-              <p class="t-center">Fictional University offers lunch plans for those in need.</p>
-              <p class="t-center no-margin"><a href="#" class="btn btn--blue">Learn more</a></p>
-            </div>
-          </div>
-        </div>              
-      </div>
+      <?php 
+            $homepageHeroSlides = new WP_Query(array(
+              'posts_per_page' => 3,
+              'post_type' => 'hero-slide'
+            ));
+                
+            while($homepageHeroSlides->have_posts()){
+              $homepageHeroSlides->the_post(); ?>    
+              
+              <div class="hero-slider__slide" style="background-image: url(<?php echo get_the_post_thumbnail_url(); ?>);">
+                <div class="hero-slider__interior container">
+                  <div class="hero-slider__overlay">
+                    <h2 class="headline headline--medium t-center"><?php the_title();?></h2>
+                    <p class="t-center"><?php echo get_post_meta(get_the_ID(), 'hero-slide_subtitle', true)?></p>
+                    <p class="t-center no-margin"><a href="#" class="btn btn--blue">Learn more</a></p>
+                  </div>
+                </div>
+              </div>
+          <?php } wp_reset_postdata();?>
+        
 
       </div>
       <div class="slider__bullets glide__bullets" data-glide-el="controls[nav]"></div>
